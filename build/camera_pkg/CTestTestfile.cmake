b0ef43e58ef147ff61d04da387c274faf273f640
Beswad <3218857921@qq.com>
2025-12-01 22:31:56 +0800

    Delete build directory

--- build/camera_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xxy/ros_competition_2025/src/camera_pkg
-# Build directory: /home/xxy/ros_competition_2025/build/camera_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
